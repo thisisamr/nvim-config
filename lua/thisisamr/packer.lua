976402c6e9e15bc12e641a02616be3eff1494e30
Amr Soliman <thisissoliman@protonmail.com>
2023-12-22 21:58:58 +0200

    added autopairs

--- lua/thisisamr/packer.lua
+++ lua/thisisamr/packer.lua
@@ -5,6 +5,20 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
 
+  use { "windwp/nvim-autopairs",
+  -- Optional dependency
+  dependencies = { 'hrsh7th/nvim-cmp' },
+  config = function()
+    require("nvim-autopairs").setup {}
+    -- If you want to automatically add `(` after selecting a function or method
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local cmp = require('cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
+  end,
+}
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  -- or                            , branch = '0.1.x',
