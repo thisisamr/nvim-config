087fcda00a35ab6754d39b62bed6f7954fae923e
Amr Soliman <thisissoliman@protonmail.com>
2024-01-03 12:10:24 +0200

    added a background coloring function

--- lua/thisisamr/init.lua
+++ lua/thisisamr/init.lua
@@ -2,6 +2,7 @@ require("thisisamr.packer")
 require("thisisamr.remap")
 require("thisisamr.telescope")
 
+require("thisisamr.colors")
 require("thisisamr.harpoon")
 require("thisisamr.undotree")
 require("thisisamr.emoji")
