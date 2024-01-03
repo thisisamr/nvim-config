5257bb30428d194a8671a8f6e0cddbd9ff0e4ef4
Amr Soliman <thisissoliman@protonmail.com>
2024-01-03 12:13:26 +0200

    added lsp

--- lua/thisisamr/packer.lua
+++ lua/thisisamr/packer.lua
@@ -25,9 +25,29 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+  --use 'neovim/nvim-lspconfig'
+  use 'simrat39/rust-tools.nvim'
   use ('nvim-treesitter/nvim-treesitter', {run= ':TSUpdate'})
   use ('nvim-treesitter/playground')
   use ( 'ThePrimeagen/harpoon')
   use ('mbbill/undotree') 
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      requires = {
+          --- Uncomment these if you want to manage LSP servers from neovim
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
 
-  end)
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-nvim-lua'},
+  }
+}
+ end)
