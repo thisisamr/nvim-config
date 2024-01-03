a7f2f9726d206e718a8369c8c8f1e699226932ec
Amr Soliman <thisissoliman@protonmail.com>
2024-01-03 12:21:13 +0200

    added nvim-surround

--- lua/thisisamr/packer.lua
+++ lua/thisisamr/packer.lua
@@ -5,6 +5,15 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
 
+  use({
+      "kylechui/nvim-surround",
+      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  })
   use("xiyaowong/telescope-emoji")
   use { "windwp/nvim-autopairs",
   -- Optional dependency
